f7fe51a7387c8b8b7e6a0a68d049b3985df9e0db
wmda <p3dh0001@gmail.com>
2019-12-21 12:42:47 -0500

    updated mac changer

--- mac.py
+++ mac.py
@@ -2,12 +2,21 @@
 
 import subprocess
 import argparse
+import random
+
+def mac_generator():
+    mac_address = "02:%02x:%02X:%02x:%02x:%02x" % (random.randint(0, 255),random.randint(0, 255),
+    random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+    return(mac_address)
 
 def get_arguments():
     parse = argparse.ArgumentParser()
-    parse.add_argument("-i", "--interface", dest='interface', help='Interface to change mac address')
-    parse.add_argument("-m","--mac", dest='mac',help='New mac address')
+    parse.add_argument("-i", "--interface", dest='interface', required=True, help='Interface to change mac address.')
+    parse.add_argument("-m","--mac", dest='mac',help='New mac address, can use --r to generate random.')
+    parse.add_argument("-r", help='Randomly assigns mac address.', action='store_true')
     options= parse.parse_args()
+    if options.r:
+        options.mac = mac_generator()
     if not options.interface:
         parse.error(">> Needs interace. Use --help for more information.")
     elif not options.mac:
@@ -20,6 +29,5 @@ def change_mac (interface, mac):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw","ether", mac])
     subprocess.call(["ifconfig", interface, "up"])
-
 options = get_arguments()
 change_mac(options.interface, options.mac)
