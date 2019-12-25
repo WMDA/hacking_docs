d9b631893746c0990efa79c55c03c46741ca35eb
wmda <p3dh0001@gmail.com>
2019-12-25 18:19:58 +0000

    network scanner

--- network_scanner.py
+++ network_scanner.py
@@ -14,9 +14,18 @@ def scan(ip):
     arp_request = scapy.ARP(pdst = ip)
     broadcast= scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    ans,unans= scapy.srp(arp_request_broadcast, timeout=1)
-    print(ans.summary())
+    ans = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    client_list=[]
+    for i in ans:
+        client_dic={'IP':i[1].psrc, 'MAC':i[1].hwsrc}
+        client_list.append(client_dic)
+    return client_list
 
+def output(results_list):
+    print('','-'*100,'\n',"\t IP \t\t\tMac address",'\n','-'*100)
+    for i in results_list:
+        print('\t',i['IP'] + "\t\t" + i['MAC'])
 
 options = ip()
-scan(options.ip)
+scan_results=scan(options.ip)
+output(scan_results)
