f98d6bda325eae426efb94aa04370bfad6b425ed
WMDA <p3dh0001@gmail.com>
2020-07-10 14:14:59 +0100

    Updated arp and network scanner with interface

--- network_scanner.py
+++ network_scanner.py
@@ -2,22 +2,27 @@
 
 import scapy.all as scapy
 import argparse
+from datetime import datetime
+import sys
 
 def ip():
     parse = argparse.ArgumentParser()
     parse.add_argument("-ip", dest="ip", help="Needs IP range /24")
+    parse.add_argument("-i", dest="interface", help='Needs interface')
     options= parse.parse_args()
-    if not options.ip: 
+    if not options.ip:
         parse.error('>> Needs ip address. Use -h for further details.')
+    elif not options.interface:
+        parse.error('>> Needs interface. Use -h for further details')
     else:
         return options
 
 
-def scan(ip):
+def scan(ip,interface):
     arp_request = scapy.ARP(pdst = ip)
     broadcast= scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    ans = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    ans = scapy.srp(arp_request_broadcast, timeout=1,iface=interface, verbose=False)[0]
     client_list=[]
     for i in ans:
         client_dic={'IP':i[1].psrc, 'MAC':i[1].hwsrc}
@@ -30,5 +35,15 @@ def output(results_list):
         print('\t',i['IP'] + "\t\t" + i['MAC'])
 
 options = ip()
-scan_results=scan(options.ip)
-output(scan_results)
+try:
+    print('\nScanning please wait:\n ')
+    start=datetime.now()
+    scan_results=scan(options.ip, options.interface)
+    stop=datetime.now()
+    duration= stop-start
+    output(scan_results)
+    print('-'*100,'\nScan Complete\n')
+    print('Scan duration: %s'%(duration))
+except KeyboardInterrupt:
+    print('User requested shut down:')
+    sys.exit()
